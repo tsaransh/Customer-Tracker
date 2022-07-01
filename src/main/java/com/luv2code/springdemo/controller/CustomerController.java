65437dc09e350fa9f98e440c9dec4e33d83882bb
Saransh Tyagi <tyagisaransh90@gmail.com>
2022-07-01 21:37:01 +0530

    add service before DAO

--- src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -5,24 +5,27 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.luv2code.springdemo.dao.CustomerDAO;
+
 import com.luv2code.springdemo.entity.Customer;
+import com.luv2code.springdemo.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
-	// need to inject the customer DAO
+	// need to inject the customer service
 	
 	@Autowired
-	private CustomerDAO customerDAO;
+	private CustomerService customerService;
 	
-	@RequestMapping("/list")
+	
+	@GetMapping("/list")
 	public String listCustomer(Model theModel) {
 		// get customer from the DAO
-		List<Customer> theCustomer = customerDAO.getCustomers();
+		List<Customer> theCustomer = customerService.getCustomers();
 		
 		// add the customers to the model
 		theModel.addAttribute("customers", theCustomer);
