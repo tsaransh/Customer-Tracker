b2e9264868631daa9374045a6628ca62dc0d2a21
Saransh Tyagi <tyagisaransh90@gmail.com>
2022-07-01 17:21:01 +0530

    make change in customer-list.jsp

--- src/main/java/com/luv2code/springdemo/controller/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -1,16 +1,32 @@
 package com.luv2code.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.luv2code.springdemo.dao.CustomerDAO;
+import com.luv2code.springdemo.entity.Customer;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 
+	// need to inject the customer DAO
+	
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
 	public String listCustomer(Model theModel) {
-		System.out.println("FIRST REQUEST RECEIVED");
+		// get customer from the DAO
+		List<Customer> theCustomer = customerDAO.getCustomers();
+		
+		// add the customers to the model
+		theModel.addAttribute("customers", theCustomer);
+		
 		return "list-customers";
 	}
 	
